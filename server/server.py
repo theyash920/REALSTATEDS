7ec2310d69b0aba38813fc828cfaa009f880125e
Yash Chopra <theyash920@gmail.com>
2025-07-24 15:33:19 +0530

    initial commit

---
+++ server/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify 
+import util
+app = Flask(__name__)
+
+@app.route('get location names')
+def get_location_names():
+    response = jsonify({
+        "locations": util.get_location_names()
+    })
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+    return "Hi"
+if __name__ == "__main__":
+    print("Starting Python Flask Server")
+    app.run()
